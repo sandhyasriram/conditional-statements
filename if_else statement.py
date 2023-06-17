28dd164670d4c182077d2fc2552022c5562ad0f6
sandhyasriram <sandhyasriram1621@gmail.com>
2023-06-17 15:29:10 +0530

    I created a list from 8th line.

---
+++ if_else statement.py
@@ -0,0 +1,16 @@
+a = 10
+if a < 5:
+    print("a is less than 5")
+else:
+    print("a is grater than 5")
+
+
+# create a list
+
+list = [1, 2, 3, 4, 5, 6]
+sum = 0
+
+for num in list:
+    sum += num
+print("sum of all the elements in list:", sum)
+
